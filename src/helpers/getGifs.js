4a709b2949974d69559d081bf754dd0058710957
Jose Cabello <jmcabelloquispe@gmail.com>
2021-06-27 02:52:54 -0500

    index.html updated

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
   console.log(category);
-  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=yiZN8zZVNSI4ce7SDvdtyAQNBXw0lMWe`
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=yiZN8zZVNSI4ce7SDvdtyAQNBXw0lMWe`
   const resp = await fetch(url);
   const {data} = await resp.json();
 
